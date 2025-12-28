f176fb70404fe262feb66d75e728f2b02a6cdd6a
Indranil102e <indranilsamanta2003@gmail.com>
2025-12-28 17:54:15 +0530

    added the ml file and the data

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from optparse import Option
+
 import streamlit as st
+import pickle
+import pandas as pd
+
+def recommend(movie):
+    movie_index= new_df[new_df['title']==movie].index[0]
+    distances=similarity[movie_index]
+    movie_list=sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:6]
+
+    for i in movie_list:
+        print(new_df.iloc[i[0]].title)
+        
+        
+
+movies_dict=pickle.load(open('movies.pkl','rb'))
+movies=pd.DataFrame(movies_dict)
+
+
 
 st.title("Movie Recommendation System")
 
-Option=st.selectbox(
+Select_movie_name=st.selectbox(
     'Select an option:',
-    ('Home', 'Recommend Movies', 'About')
+    movies['title'].values
 ) 
+
+if st.button('Recommend'):
+    recommend(Select_movie_name)
+    st.write("Recommended movies:", selected_movie_recommens)
+    
